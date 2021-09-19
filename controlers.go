b36c8cbf8dcdadaf5375afac950ab7c841d394a8
Gendina Nina <ng@level.travel>
2021-09-19 23:05:04 +0300

    Refinement of the logic in the framework of mvc

--- controlers.go
+++ controlers.go
@@ -5,43 +5,15 @@ import (
   "net/http"
   "html/template"
   "github.com/gorilla/mux"
-  "database/sql"
 
 	_ "github.com/go-sql-driver/mysql"
 )
 
 func index(w http.ResponseWriter, r *http.Request) {
-  t, err := template.ParseFiles("templates/index.html", "templates/header.html", "templates/footer.html", "templates/user_card.html")
+  t, _ := template.ParseFiles("templates/index.html", "templates/header.html",
+    "templates/footer.html", "templates/user_card.html")
 
-  if err != nil {
-    fmt.Fprintf(w, err.Error())
-  }
-
-  var users = []User{}
-
-  db, err := sql.Open("mysql", "root:root@tcp(127.0.0.1:8889)/sosedi")
-
-  if err != nil {
-    fmt.Fprintf(w, err.Error())
-  }
-
-  defer db.Close()
-
-  res, err := db.Query("SELECT id, name, price_min, price_max FROM users ")
-
-  if err != nil {
-    fmt.Fprintf(w, err.Error())
-  }
-
-   for res.Next() {
-     var user User
-     err = res.Scan(&user.Id, &user.Name, &user.PriceMin, &user.PriceMax)
-     if err != nil {
-       fmt.Fprintf(w, err.Error())
-     }
-
-      users = append(users, user)
-   }
+  users := get_users()
 
   t.ExecuteTemplate(w, "index", users)
 }
@@ -49,35 +21,10 @@ func index(w http.ResponseWriter, r *http.Request) {
 func profile(w http.ResponseWriter, r *http.Request) {
   vars := mux.Vars(r)
 
-  t, err := template.ParseFiles("templates/profile.html", "templates/header.html", "templates/footer.html")
+  t, _ := template.ParseFiles("templates/profile.html", "templates/header.html",
+     "templates/footer.html")
 
-  if err != nil {
-    fmt.Fprintf(w, err.Error())
-  }
-
-  db, err := sql.Open("mysql", "root:root@tcp(127.0.0.1:8889)/sosedi")
-
-  if err != nil {
-    fmt.Fprintf(w, err.Error())
-  }
-
-  defer db.Close()
-
-  res, err := db.Query(fmt.Sprintf("SELECT id, name, price_min, price_max FROM users WHERE id = %s ", vars["id"]))
-
-  if err != nil {
-    fmt.Fprintf(w, err.Error())
-  }
-
-  var user = User{}
-  for res.Next() {
-     var new_user User
-     err = res.Scan(&new_user.Id, &new_user.Name, &new_user.PriceMin, &new_user.PriceMax)
-     if err != nil {
-       fmt.Fprintf(w, err.Error())
-     }
-     user = new_user
-  }
+  user := get_user(vars["id"])
 
   t.ExecuteTemplate(w, "profile", user)
 }
--- models.go
+++ models.go
@@ -1,8 +1,47 @@
 package main
 
+import (
+  "fmt"
+  "database/sql"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
 type User struct {
   Id uint16
   Name, Surname string
   Age uint16
   PriceMin, PriceMax uint32
+}
+
+func get_users() []User {
+  users := []User{}
+
+  db, _ := sql.Open("mysql", "root:root@tcp(127.0.0.1:8889)/sosedi")
+  defer db.Close()
+
+  res, _ := db.Query("SELECT id, name, price_min, price_max FROM users ")
+
+   for res.Next() {
+     var user User
+     res.Scan(&user.Id, &user.Name, &user.PriceMin, &user.PriceMax)
+     users = append(users, user)
+   }
+
+   return users
+}
+
+func get_user(id string) User {
+  db, _ := sql.Open("mysql", "root:root@tcp(127.0.0.1:8889)/sosedi")
+  defer db.Close()
+
+  res, _ := db.Query(fmt.Sprintf(
+    "SELECT id, name, price_min, price_max FROM users WHERE id = %s ", id))
+
+    var user User
+    for res.Next() {
+      res.Scan(&user.Id, &user.Name, &user.PriceMin, &user.PriceMax)
+    }
+
+  return user
 }
